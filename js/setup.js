0a4775ea5ff7d236196a40ef16b7945f1317f554
Alexandr Timoshenskiy <timoshenskiy.ad@gmail.com>
2020-01-26 13:04:53 +0300

    Исправление недочетов

--- js/setup.js
+++ js/setup.js
@@ -3,13 +3,14 @@
 document.querySelector('.setup').classList.remove('hidden');
 // 2. Создаем массив с объектами, которые описывают магов.
 // 2.1 Задаем константы для генерации случайных магов
+var COUNT_OF_WIZARDS = 4;
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb (0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 // 2.2 Функция для выбора случайного элемента из массива
-var chooseRandomProperty = function (arr) {
-  return arr[Math.floor(Math.random() * (arr.length))];
+var chooseRandomProperty = function (array) {
+  return array[Math.floor(Math.random() * (array.length))];
 };
 // 2.3 Функция для создания случайного мага
 var createRandomWizard = function (names, lastNames, coatColors, eyesColors) {
@@ -19,9 +20,9 @@ var createRandomWizard = function (names, lastNames, coatColors, eyesColors) {
   return {name: name, coatColor: coatColor, eyesColor: eyesColor};
 };
 // 2.4 Создаем массив из четырех случайных магов
-var RandomFourWizards = [];
-for (var i = 0; i < 4; i++) {
-  RandomFourWizards[i] = createRandomWizard(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
+var randomWizards = [];
+for (var i = 0; i < COUNT_OF_WIZARDS; i++) {
+  randomWizards[i] = createRandomWizard(NAMES, LAST_NAMES, COAT_COLORS, EYES_COLORS);
 }
 // 3.1 Находим блок, который мы будем наполнять случайными сгенерированными магами
 var similarListElement = document.querySelector('.setup-similar-list');
@@ -39,8 +40,8 @@ var renderWizard = function (wizard) {
 };
 // 4.1 Создаем пустой объект документа, который мы будем наполнять, перед добавлением в DOM
 var fragment = document.createDocumentFragment();
-for (i = 0; i < 4; i++) {
-  fragment.appendChild(renderWizard(RandomFourWizards[i]));
+for (i = 0; i < randomWizards.length; i++) {
+  fragment.appendChild(renderWizard(randomWizards[i]));
 }
 // 4.2 Добавляем наполненный фрагмент в DOM
 similarListElement.appendChild(fragment);
